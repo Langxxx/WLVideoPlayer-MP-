537900aae98db2dc63c85f1cafa8b2b5135ce189
SmallLang <316230604@qq.com>
2016-02-24 11:17:48 +0800

    Initial Commit

---
+++ WLVideoPlayer(MP)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WLVideoPlayer(MP)
+//
+//  Created by wl on 16/2/24.
+//  Copyright © 2016年 wl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
